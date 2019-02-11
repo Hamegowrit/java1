9eca8705570914d49a6ef4625324cbb710dc7cfd
Hamegowrit <43901594+Hamegowrit@users.noreply.github.com>
2019-02-11 14:03:56 +0530

    Update VowelConsonant.java

--- VowelConsonant.java
+++ VowelConsonant.java
@@ -2,7 +2,7 @@ public class VowelConsonant {
 
     public static void main(String[] args) {
 
-        char ch = 'a';
+        char ch = 'f';
 
         if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
             System.out.println(ch + " is vowel");
